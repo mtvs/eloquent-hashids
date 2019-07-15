158bd738722ff1696866ff50f5ae30a4f61b710c
Mohammad Ali Tavassoli <mtvs.dev@gmail.com>
2019-07-15 23:55:28 +0430

    Generate hashid

--- src/HasHashid.php
+++ src/HasHashid.php
@@ -6,5 +6,14 @@ use Vinkla\Hashids\Facades\Hashids;
 
 Trait HasHashid 
 {
-	
+	public function hashid()
+	{
+		return Hashids::connection($this->getHashidsConnection())
+			->encode($this->getKey());
+	}
+
+	public function getHashidsConnection()
+	{
+		return config('hashids.default');
+	}
 }
--- tests/HasHashidTest.php
+++ tests/HasHashidTest.php
@@ -8,5 +8,22 @@ use Vinkla\Hashids\Facades\Hashids;
 
 class HasHashidTest extends TestCase 
 {
+	use RefreshDatabase;
 
+	/**
+	 * @test
+	 */
+	public function it_can_generate_the_hashid()
+	{
+		$item = Item::create();
+		$hashId = $item->hashid();
+		$decoded = Hashids::connection(
+			$item->getHashidsConnection()
+		)->decode($hashId)[0];
+
+		$this->assertEquals(
+			$item->getKey(),
+			$decoded
+		);
+	}
 }
