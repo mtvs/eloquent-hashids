0801deaf2b0afd03b5d41824b5070fc6380432dc
Le Dinh <64189902+dinhdjj@users.noreply.github.com>
2022-06-08 16:37:50 +0700

    support child route binding

--- src/HashidRouting.php
+++ src/HashidRouting.php
@@ -2,6 +2,9 @@
 
 namespace Mtvs\EloquentHashids;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
 trait HashidRouting
 {
 	/**
@@ -15,6 +18,25 @@ trait HashidRouting
 
 		return $this->findByHashid($value);
 	}
+
+	/**
+    * @see parent
+    */
+    public function resolveChildRouteBinding($childType, $value, $field)
+    {
+        $relationship = $this->{Str::plural(Str::camel($childType))}();
+
+		if(!($relationship instanceof Model)){
+			$relationship = $relationship->getRelated();
+		}
+
+        if (null === $field && \in_array(HashidRouting::class, class_uses_recursive($relationship), true)) {
+			$value = $relationship->hashidToId($value);
+			$field = $relationship->getKeyName();
+        }
+
+        return parent::resolveChildRouteBinding($childType, $value, $field);
+    }
 
 	/**
 	 * @see parent
